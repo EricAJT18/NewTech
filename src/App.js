29325164b4a38b26214ae749c937f4a2af9cd088
ericjimenez2012@gmail.com <EricReposGit01>
2021-02-08 12:41:39 -0400

    Commit Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import { AddEmployee } from "./components/AddEmployee";
+import { EditEmployee } from "./components/EditEmployee";
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import {GlobalProvider} from "./context/GlobalState";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxWidth: "30rem", margin: "4rem auto" }}>
+      <GlobalProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/add" component={AddEmployee} />
+            <Route path="/edit/:id" component={EditEmployee} />
+          </Switch>
+        </Router>
+      </GlobalProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
